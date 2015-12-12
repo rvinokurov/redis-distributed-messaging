445118a8bcb7e8d1fab7c6abd1b917c181f45207
rvinokurov <r4shed@gmail.com>
2015-12-12 23:42:05 +0300

    change node verify algoritm

--- app.js
+++ app.js
@@ -3,92 +3,162 @@
 const minimist = require('minimist');
 const redis = require('./lib/redis.js');
 const logger = require('bunyan').createLogger({name: 'redis-distributed-messaging'});
-
+const _ = require('underscore');
+const os = require('os');
+const async = require('async');
+var argv = require('minimist')(process.argv.slice(2));
 
 const pid = process.pid;
 
 logger.info('app started', {pid});
 
-const publisher = redis.getConnection();
-const subscriber = redis.getConnection();
-
-
-let master_pid = null;
+let publisher;
+let subscriber;
 
 let isMaster = false;
 
+const nodeName = `${os.hostname()}_${pid}`;
 
-let masterTimeout = null;
-let searchMasterTimeout = null;
-let recieved_master_pid = null;
-const listenWhoIsMaster = () => {
-    let masterMessage = {
-        'type': 'I_AM_MASTER',
-        pid
-    };
+const processPingMessage = (message) => {
+    console.log(message);
+    publisher.publish(`${message.node}:pong`, JSON.stringify({node: nodeName}));
+    console.log('pong from node', nodeName);
+};
 
-    subscriber.on('message', (channel, message) => {
-        if(channel === 'system') {
-            message = JSON.parse(message);
-        }
-        if(message.type === 'WHO_IS_MASTER' && isMaster ) {
-            publisher.publish('system', JSON.stringify(masterMessage));
-        }
-    });
-}
+const processNewNodeMessage = (message) => {
+    nodeList.push(message.node);
+    nodeInfo[message.node] = 0;
+    logger.info('registered new node. Node List Now', nodeList)
+};
 
-const detectMaster = () => {
-    let message = {
-        'type': 'WHO_IS_MASTER',
-        pid
-    };
 
-    setInterval(function() {
-        recieved_master_pid = null;
-        publisher.publish('system', JSON.stringify(message));
-        clearTimeout(masterTimeout);
-        masterTimeout = setTimeout(function() {
-            if(!recieved_master_pid) {
-                isMaster = true;
-                master_pid = pid;
-                console.log('i am master', pid);
-            }
-        }, 500);
-    }, 500);
+const processKillMessage = (message) => {
+    logger.info('Recieved kill message. Shutdown');
+    process.exit(0);
+};
 
 
 
+const processMessages = (channel, message) => {
+    console.log(channel, message);
+    try {
+        message = JSON.parse(message);
+    } catch (e) {
+        logger.error('Error on parse message', {error: e})
+    }
 
+    if(message.node === nodeName) {
+        return;
+    }
+    if(channel === `${nodeName}:ping`) {
+        processPingMessage(message);
+    }
+    if(channel === 'newnode') {
+        processNewNodeMessage(message);
+    }
+    if(channel === 'kill') {
+        processKillMessage(message);
+    }
+};
 
-    subscriber.on('message', (channel, message) => {
-        // console.log(channel, message);
-        if(channel === 'system') {
-            message = JSON.parse(message);
-        }
-        if(message.type === 'I_AM_MASTER') {
-            recieved_master_pid = message.pid;
-            if(master_pid != message.pid) {
-                master_pid = message.pid;
-                console.log('master', master_pid);
-            }
-            clearTimeout(masterTimeout);
-        }
+const initRedisConnections  = (callback) => {
+    async.parallel({
+        publisher: redis.getConnection.bind(null, 'publisher'),
+        subscriber: redis.getConnection.bind(null,'subscriber')
+    }, (error, result) => {
+        publisher = result.publisher;
+        subscriber = result.subscriber;
+        callback(error);
     });
 };
 
-const init = () => {
-    listenWhoIsMaster();
-    detectMaster();
-
-
+const getNextNode = () => {
+    let myIndex = _.indexOf(nodeList, nodeName);
+    return myIndex + 1 == nodeList.length ? nodeList[0] : nodeList[myIndex + 1];
 };
 
-subscriber.on("subscribe", function(channel, count) {
-    console.log("Subscribed to " + channel + ". Now subscribed to " + count + " channel(s).");
-    init();
+const tryToPingNextNode = () => {
+    let nextNode = getNextNode();
+    if(nextNode === nodeName) {
+        return;
+    } else {
+        publisher.publish(`${nextNode}:ping`, JSON.stringify({node: nodeName}));
+        console.log('ping node', nextNode);
+    }
+};
 
 
-});
+
+let nodeList = [];
+let nodeInfo = [];
+
+async.series([
+    initRedisConnections,
+    (callback) => {
+        if(argv.flush) {
+            logger.info('Clear node information, send kill message to other nodes..');
+            publisher.flushdb(callback);
+            publisher.publish('kill', JSON.stringify({node: nodeName}));
+        } else {
+            callback(null);
+        }
+    },
+    (callback) => {
+        async.parallel({
+            nodeList : publisher.get.bind(publisher, 'nodelist'),
+            nodeInfo : publisher.get.bind(publisher, 'nodeinfo')
+        }, (error, result) => {
+            if(result.nodeList) {
+                try {
+                    nodeList = JSON.parse(result.nodeList);
+                    nodeInfo = JSON.parse(result.nodeInfo);
+                } catch(e) {
+                    nodeList = []
+                    nodeInfo = {}
+                }
+            } else {
+                nodeList = []
+                nodeInfo = {}
+            }
 
 
-subscriber.subscribe('system');
+            callback(error);
+        })
+
+    },
+    (callback) => {
+        subscriber.on("subscribe", (channel, count)  => {
+            logger.info("Subscribed to " + channel + ". Now subscribed to " + count + " channel(s).");
+        });
+        callback(null);
+    },
+    callback => subscriber.subscribe(`${nodeName}:ping`, callback),
+    callback => subscriber.subscribe(`newnode`, callback),
+    callback => subscriber.subscribe(`kill`, callback),
+    (callback) => {
+        subscriber.on('message', processMessages)
+        callback(null);
+    },
+    (callback) => {
+        console.log(nodeList);
+        if(!nodeList.length) {
+            logger.info('start message generator');
+            isMaster = true;
+        } else {
+            console.log('start message listener');
+            publisher.publish('newnode', JSON.stringify({node: nodeName}));
+        }
+        nodeList.push(nodeName);
+        nodeInfo[nodeName] = isMaster;
+
+
+        publisher.set('nodelist', JSON.stringify(nodeList));
+        publisher.set('nodeinfo', JSON.stringify(nodeInfo));
+
+        tryToPingNextNode();
+        setInterval(tryToPingNextNode, 3000);
+    }
+
+
+
+]);
