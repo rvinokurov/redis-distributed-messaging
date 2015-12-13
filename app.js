25fe6997ca2d2d840bb80b2b4dc779bd0c21c474
rvinokurov <r4shed@gmail.com>
2015-12-13 15:36:23 +0300

    refactor (WIP)

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const async = require('async');
 var argv = require('minimist')(process.argv.slice(2));
 const MessageEmitter = require('./lib/message-emitter.js');
 const MessageManager = require('./lib/message-manager.js');
+const NodeManager = require('./lib/node-manager.js');
+
 
 let scope = {
     nodeList: [],
@@ -28,14 +30,8 @@ scope.nodeName = `${os.hostname()}_${process.pid}`;
 
 let messageEmitter = null;
 let messageManager = null;
+let nodeManager = null;
 
-
-
-
-// const processPongMessage = (message) => {
-//     clearTimeout(nextNodePingTimeout);
-//     setTimeout(resetPing, 3000);
-// };
 
 
 
@@ -50,51 +46,6 @@ const initRedisConnections  = (callback) => {
     });
 };
 
-const getNextNode = () => {
-    let nodeList = scope.nodeList,
-        nodeName = scope.nodeName;
-    let myIndex = _.indexOf(nodeList, nodeName);
-    return myIndex + 1 == nodeList.length ? nodeList[0] : nodeList[myIndex + 1];
-};
-
-const tryToPingNextNode = (next) => {
-    let nodeName = scope.nodeName,
-        nodeList = scope.nodeList,
-        nodeInfo = scope.nodeInfo,
-        publisher = scope.publisher;
-
-    let nextNode = getNextNode();
-    if(nextNode === nodeName) {
-        setTimeout(next, 1000);
-    } else {
-        let nextNodePingTimeout = setTimeout(() => {
-            console.log('node dead', nextNode);
-            nodeList = _.without(nodeList, nextNode);
-            if(scope.nodeInfo[nextNode]) {
-                scope.nodeInfo[nodeName] = true;
-                logger.info(`new master is ${nodeName}`);
-                //start broadcast
-            }
-            delete scope.nodeInfo[nextNode]; //check if nextNode is master
-
-            console.log('current node list', nodeList);
-            publisher.set('nodelist', JSON.stringify(nodeList));
-            publisher.set('nodeinfo', JSON.stringify(nodeInfo));
-            publisher.publish(`updatenodelist`, JSON.stringify({nodeList, nodeInfo}));
-            next(null);
-        }, 1000);
-        console.log('ping node', nextNode);
-        messageEmitter.once(`${nodeName}:pong`, () => {
-            clearTimeout(nextNodePingTimeout);
-            setTimeout(next, 1000);
-        });
-        publisher.publish(`${nextNode}:ping`, JSON.stringify({node: nodeName}));
-    }
-};
-
-
-
-
 async.series([
     initRedisConnections,
     (callback) => {
@@ -106,29 +57,7 @@ async.series([
             callback(null);
         }
     },
-    (callback) => {
-        async.parallel({
-            nodeList : scope.publisher.get.bind(scope.publisher, 'nodelist'),
-            nodeInfo : scope.publisher.get.bind(scope.publisher, 'nodeinfo')
-        }, (error, result) => {
-            if(result.nodeList) {
-                try {
-                    scope.nodeList = JSON.parse(result.nodeList);
-                    scope.nodeInfo = JSON.parse(result.nodeInfo);
-                } catch(e) {
-                    scope.nodeList = []
-                    scope.nodeInfo = {}
-                }
-            } else {
-                scope.nodeList = []
-                scope.nodeInfo = {}
-            }
 
-
-            callback(error);
-        })
-
-    },
     callback => {
 
         messageEmitter = new MessageEmitter(scope);
@@ -141,26 +70,12 @@ async.series([
             'updatenodelist': message => messageManager.updateNodeList(message),
         }, callback)
     },
-    (callback) => {
-        let isMaster = false;
-        if(!scope.nodeList.length) {
-            isMaster = true;
-            logger.info('start message generator');
-        } else {
-            console.log('start message listener');
-            console.log('new node');
-            scope.publisher.publish('newnode', JSON.stringify({node: scope.nodeName}));
-        }
-        scope.nodeList.push(scope.nodeName);
-        scope.nodeInfo[scope.nodeName] = isMaster;
-        console.log(scope.nodeInfo);
-        scope.publisher.set('nodelist', JSON.stringify(scope.nodeList));
-        scope.publisher.set('nodeinfo', JSON.stringify(scope.nodeInfo));
 
-        messageEmitter.subscribe([`${scope.nodeName}:pong`], () => {
-            async.forever(tryToPingNextNode)
-        });
-
-
+    callback => {
+        nodeManager = new NodeManager(scope, messageEmitter);
+        nodeManager.init(callback);
+    },
+    callback => {
+        nodeManager.watchClosestNodeStatus();
     }
 ]);
